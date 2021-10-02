f3248631d2a492070fc7893b89a8002202ddd406
Samanthi De Silva <samanthi20258@gmail.com>
2021-10-03 00:01:01 +0530

    bookitems added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import {AppBar, Toolbar, IconButton, Badge, Typography} from '@material-ui/core'
 import {ShoppingCart, BookmarksRounded} from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
 import useStyles from './styles';
+import logo from '../../assets/logos.png';
 
 const Navbar = ({totalItems}) => {
     const classes = useStyles();
@@ -13,7 +14,8 @@ const Navbar = ({totalItems}) => {
          <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography component={Link} to="/" variant="h4" className={classes.title} color="inherit">
-            <BookmarksRounded style={{ fontSize: 45 }}/>
+            {/* <BookmarksRounded style={{ fontSize: 45 }}/> */}
+            <img src={logo} alt="Book Store App" height="50px" />
               <strong >Fantastic Book Store</strong> 
           </Typography>
 
